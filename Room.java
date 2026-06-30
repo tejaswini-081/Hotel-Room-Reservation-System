4183b285d2bbe77dc6e6bf3e0b26f99256da8a83
tejaswini-081 <tejugopal10@gmail.com>
2026-06-30 10:57:29 +0530

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,17 +1,45 @@
 public class Room {
-    int roomNo;
-    String roomType;
-    boolean isBooked;
+    private int roomNumber;
+    private String roomType;
+    private double price;
+    private boolean booked;
 
-    public Room(int roomNo, String roomType) {
-        this.roomNo = roomNo;
+    public Room(int roomNumber, String roomType, double price) {
+        this.roomNumber = roomNumber;
         this.roomType = roomType;
-        this.isBooked = false;
+        this.price = price;
+        this.booked = false;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isBooked() {
+        return booked;
+    }
+
+    public void bookRoom() {
+        booked = true;
+    }
+
+    public void cancelBooking() {
+        booked = false;
     }
 
     public void displayRoom() {
-        System.out.println("Room No: " + roomNo);
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Booked: " + isBooked);
+        System.out.println("Room No : " + roomNumber);
+        System.out.println("Type    : " + roomType);
+        System.out.println("Price   : ₹" + price);
+        System.out.println("Status  : " + (booked ? "Booked" : "Available"));
+        System.out.println("----------------------------");
     }
 }
